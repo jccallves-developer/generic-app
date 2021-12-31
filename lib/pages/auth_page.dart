8cbcd3d287013547f7ac69717cc6c590c6908644
jccallves <jccallves@macae.ufrj.br>
2021-12-31 13:33:32 -0300

    Implementaçao de autenticaçao usando dados mockados.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,7 +1,7 @@
 import 'package:chat/components/auth_form.dart';
-import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/core/models/auth_form_data.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as dev;
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -13,13 +13,30 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
 
-    dev.log('AuthPage...');
-    dev.log(formData.email);
-
-    setState(() => _isLoading = false);
+      if (formData.isLogin) {
+        // Login
+        await AuthService().login(
+          formData.email,
+          formData.password,
+        );
+      } else {
+        // Signup
+        await AuthService().signup(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
+      }
+    } catch (error) {
+      // Tratar erro!
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
