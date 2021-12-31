208c3aee32ed5a217b76a5df899ca46f61ace95c
jccallves <jccallves@macae.ufrj.br>
2021-12-31 14:48:38 -0300

    Configuraçao do firebase autentication, configuraçao da inicializaçao do firebase usando flutterfire configure e autenticaçao salva no Firebase Autentication.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:chat/firebase_options.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
